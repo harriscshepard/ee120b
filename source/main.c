723abcbaca91ab496a60138bcf2e96be0eeb6dcc
Harris Shepard <hshep002@wch136-24.cs.ucr.edu>
2020-10-20 17:22:40 -0700

    Exercise 1

--- source/main.c
+++ source/main.c
@@ -15,8 +15,27 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
 
+	DDRA = 0x00;
+	PORTA = 0xFF;
+	DDRB = 0xFF;
+	PORTB = 0x00;
+
+	char PA0 = 0x00;
+	char PA1 = 0x00;
+	char PB0 = 0x00;
     /* Insert your solution below */
     while (1) {
+	PA0 = PINA & 0x01;
+	PA1 = PINA & 0x02;
+	if(!PA1 && PA0)
+	{
+		PB0 = 0x01;
+	}
+	else
+	{
+		PB0 = 0x00;
+	}
+	PORTB = PB0;
 
     }
     return 1;
