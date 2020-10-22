b2eb2fb60aa97fbc04e58ad3cdac90f4c1b66710
Harris Shepard <hshep002@wch136-24.cs.ucr.edu>
2020-10-22 12:59:56 -0700

    Lab4 exercise 1: Added intializations to main

--- turnin/hshep002_lab4_part1.c
+++ turnin/hshep002_lab4_part1.c
@@ -66,6 +66,10 @@ int main(void) {
 	DDRC = 0xFF;
 	PORTC = 0x00;
 
+	PB0 = 1;
+	PB1 = 0;
+	State = zeroth_led_on;
+
 	unsigned char fuelLevel = 0x00;
 	unsigned char pa4 = 0x00;// for exercise 3
 	 unsigned char pa5 = 0x00;  
@@ -76,71 +80,8 @@ int main(void) {
    while (1) {
 //	pA0 = PINA & 0x01;
 //	pA1 = PINA & 0x02;
-//	lab3 counting 1's in ports
-	fuelLevel = 0x00;
-	switch(PORTA) {
-	case 1:
-	case 2:
-		//pc5 lights
-		fuelLevel = 0x20;
-		break;
-	case 3:
-	case 4:
-		//pc4 and 5
-		fuelLevel = 0x30;
-		break;
-	case 5:
-	case 6:
-		//pc 5,4 3 ,
-		fuelLevel = 0x38;
-		break;
-	case 7:
-	case 8:
-	case 9:
-		//pc 5 4 3 2
-		fuelLevel = 0x3C;
-		break;
-	case 10:
-	case 11:
-	case 12:
-		//pc 5 4 3 2 1
-		fuelLevel = 0x3E;
-		break;
-	case 13:
-	case 14:
-	case 15:
-		fuelLevel = 0x3F;
-		break;
-	default:
-		fuelLevel = 0x00;
-		break;
-	}
-	//low fuel warning
-	if(PORTA < 5)//if 4 or less
-	{
-		fuelLevel = fuelLevel | 0x40;//pc6
-	}	
-
-	//Fasten seatbelt icon pc7, if pa4, pa5 and !pa6
-	//pa4 if key is in ignition
-	//pa5 if driver is seated
-	//pa6if seatbelt is fastened
-	if(PORTA & 0x40)
-	{
-		pa6 = 0x01;
-	}	
-	if(PORTA & 0x20)
-        {
-                pa5 = 0x01;
-        }
-	if(PORTA & 0x10)                                                                                                     {
-                pa4 = 0x01;                                                                                                  }
 	
-	if(pa4 && pa5 && !pa6)
-	{
-		fuelLevel = fuelLevel | 0x80; //set pc7
-	}
-	PORTC = fuelLevel;
+	Tick();	
 
     }
     return 1;
